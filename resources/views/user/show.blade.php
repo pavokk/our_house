f6cf85489d7377816a8ff357ba16d22c61e1b4f5
Paul Volden <pavokk@gmail.com>
2025-04-13 10:30:22 +0200

    UI changes on profile pages

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,18 +2,37 @@
 
     <main class="max-w-3xl m-auto mt-5">
 
-        <div class="profile-header flex justify-between items-center bg-lightbrown p-5 rounded-lg">
-            <h1 class="text-2xl">{{ $user->name }}s profil</h1>
-            <div class="buttons w-20">
-                @if (auth()->id() == $user->id)
-                    <x-ui.primary-button link="{{ route('user.edit') }}">Rediger</x-main-btn>
-                @endif
-            </div>
-        </div>
+        <x-ui.primary-box class="profile-header flex justify-between items-center">
 
-        <h1>{{ $user->name }}</h1>
-        <p>Email: {{ $user->email }}</p>
-        <p>Joined: {{ $user->created_at->diffForHumans() }}</p>
+            <div class="pic-and-name flex justify-start items-center gap-4">
+                <div class="profile-pic w-16 h-16">
+                    <x-users.profile-picture :imageLocation="asset('storage/' . $user->image->image)" :altText="$user->image->alt" />
+                </div>
+                <h1 class="text-2xl">{{ $user->name }}s profil</h1>
+            </div>
+
+            @auth
+                @if (auth()->id() == $user->id)
+                    <div class="edit-user w-20">
+                        <x-ui.primary-button link="{{ route('user.edit') }}">Rediger</x-main-btn>
+                    </div>
+                @endif
+            @endauth
+
+
+        </x-ui.primary-box>
+
+        <x-ui.primary-box class="desc-box">
+
+            <p>
+                @if (isset($user->description))
+                    {{ $user->description }}
+                @else
+                    Ingenting her...
+                @endif
+            </p>
+            
+        </x-ui.primary-box>
 
     </main>
     
